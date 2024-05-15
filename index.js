7635c77da04a05cf9d2d861a18d23de85b9b76df
NinjaGrandpa <34811547+NinjaGrandpa@users.noreply.github.com>
2024-05-15 16:44:03 +0200

    test

--- index.js
+++ index.js
@@ -36,21 +36,25 @@ button.addEventListener("click", () => {
 });
 
 window.addEventListener("load", () => {
-	fetch("https://api.linkedin.com/rest/memberSnapshotData?q=criteria", {
-		method: "GET",
-		mode: "cors",	
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": `Bearer ${token}`,
-			"LinkedIn-Version": 202312
-		},
-		cache: "default"
-	}).then((response) => response.text())
-		.catch((error) => console.error(error))
-		.finally((data) => {
-			document.getElementById("profile-text").innerHTML = data ?? "No Data";
-		})
 
+	const myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+	myHeaders.append("LinkedIn-Version", "202312");
+	myHeaders.append("Authorization", `Bearer ${token}`);
+	myHeaders.append("Cookie", "lidc=\"b=VB08:s=V:r=V:a=V:p=V:g=5045:u=899:x=1:i=1715781943:t=1715867589:v=2:sig=AQFYYlmJdcGaKA6dev1BKm0PVFHYF2cq\"; bcookie=\"v=2&909be48a-e8a5-41c3-80cb-00e8a3eef304\"; li_gc=MTswOzE3MTU3NjM1MjU7MjswMjFFBsulZUqE0Tk5bg4Oa0KGc+IP1EMevuxv5Wnn4fOCzg==");
+
+	const options = {
+		method: "GET",
+		headers: myHeaders,
+		redirect: "follow"
+	}
+
+	fetch("https://api.linkedin.com/rest/memberSnapshotData?q=criteria", options)
+		.then((response) => response.text())
+		.then((result) => {
+			document.getElementById("profile-text").innerHTML = result ?? "No Data";
+		})
+		.catch((error) => console.error(error))
 });
 
 
